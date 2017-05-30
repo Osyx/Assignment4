6dfff86beee832454c0858acb1d2f75973006636
Osyx <ofalkman@gmail.com>
2017-05-30 19:45:30 +0200

    Created a "solution".

--- Assignment4/life.cpp
+++ Assignment4/life.cpp
@@ -7,19 +7,46 @@
 #include <gecode/driver.hh>
 #include <gecode/int.hh>
 #include <gecode/minimodel.hh>
+#include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace Gecode;
+using namespace std;
 
 class GameOfLife : public Script {
 public:
-	// Position of queens on boards
-	IntVarArray q;
-	// The actual problem
-	GameOfLife(const SizeOptions& opt) : Script(opt), q(*this, opt.size(), 0, opt.size() - 1) {
+	int m;
+	// Array representing the matrix.
+	BoolVarArray q;
+	// Matrix representing the board
+	GameOfLife(const SizeOptions& opt) : Script(opt), m(opt.size() + 4), q(*this, m * m, 0, 1) {
 		const int n = q.size();
 		
+		Matrix<BoolVarArgs> mat(q, m, m);
+
+		for (int i = 0; i < m; ++i) {
+			for (int j = 0; j < m; ++j) {
+				if(i < 2 || j < 2 || i > m - 3 || j > m - 3)
+				rel(*this, mat(i, j), IRT_EQ, 0);
+			}
+		}
+
+		for (int i = 2; i < m - 2; ++i) {
+			for (int j = 2; j < m - 2; ++j) {
+				//if(!mat(i, j).assigned())
+					//rel(*this, mat(i, j), IRT_EQ, 1);
+				if (mat(i, j).assigned() && mat(i, j).val() == 1)  {
+					rel(*this, mat(i - 1, j + 1) + mat(i, j + 1) + mat(i + 1, j + 1) + mat(i - 1, j - 1) + mat(i, j - 1) + mat(i + 1, j - 1) + mat(i - 1, j) + mat(i + 1, j) == 3);
+					//rel(*this, mat(i - 1, j + 1) + mat(i, j + 1) + mat(i + 1, j + 1) + mat(i - 1, j - 1) + mat(i, j - 1) + mat(i + 1, j - 1) + mat(i - 1, j) + mat(i + 1, j) <= 3);
+				} else if(mat(i, j).assigned() && mat(i, j).val() == 0) {
+					rel(*this, mat(i - 1, j + 1) + mat(i, j + 1) + mat(i + 1, j + 1) + mat(i - 1, j - 1) + mat(i, j - 1) + mat(i + 1, j - 1) + mat(i - 1, j) + mat(i + 1, j) != 3);
+				}
+			}
+		}
 
 		branch(*this, q, INT_VAR_SIZE_MIN(), INT_VAL_MED());
+		//print(cout);
 	}
 
 	// Constructor for cloning \a s
@@ -36,10 +63,10 @@ public:
 	// Print solution
 	virtual void
 		print(std::ostream& os) const {
-		os << "GameOfLife\t";
+		os << "GameOfLife\n\t";
 		for (int i = 0; i < q.size(); i++) {
-			os << q[i] << ", ";
-			if ((i + 1) % 10 == 0)
+			os << q[i] << ",";
+			if ((i + 1) % m == 0)
 				os << std::endl << "\t";
 		}
 		os << std::endl;
@@ -47,10 +74,21 @@ public:
 };
 
 int main(int argc, char* argv[]) {
-	SizeOptions opt("Queens");
-	opt.iterations(500);
-	opt.size(20);
-	opt.parse(argc, argv);
-	Script::run<GameOfLife, DFS, SizeOptions>(opt);
+	bool hello = true;
+	while (hello) {
+		SizeOptions opt("GameOfLife");
+		cout << "Input the size you want for the matrix:\t 0 to quit." << endl;
+		opt.iterations(500);
+		int number = 0;
+		string input = "";
+		getline(cin, input);
+		stringstream sInput(input);
+		sInput >> number;
+		if (number == 0)
+			break;
+		opt.size(number);
+		opt.parse(argc, argv);
+		Script::run<GameOfLife, DFS, SizeOptions>(opt);
+	}
 	return 0;
 }
